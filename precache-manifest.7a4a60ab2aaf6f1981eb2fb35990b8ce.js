b5c448e590bfcd9048d9a76884b8323ac2eaaf59
Vi Zak <vizak@Vis-MacBook-Pro.local>
2020-09-28 13:16:44 +0100

    Updates

---
+++ precache-manifest.7a4a60ab2aaf6f1981eb2fb35990b8ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824200df6eb36fe055450356a60424ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0776df1c6e7850ea9e49",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9ac5c3c352c9bbddde2",
+    "url": "/static/js/2.dde2aace.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dde2aace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0776df1c6e7850ea9e49",
+    "url": "/static/js/main.1e103716.chunk.js"
+  },
+  {
+    "revision": "760b8c28a19d634a89f9",
+    "url": "/static/js/runtime-main.8e44635f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
