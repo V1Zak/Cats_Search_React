52bd6aa6b10b3f91836644497487305a93612340
Vi Zak <vizak@Vis-MacBook-Pro.local>
2020-09-28 13:18:33 +0100

    Adding 'dynamic search field' project React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchFlield: ''
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+componentDidMount(){
+  fetch ('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then (users => this.setState({monsters: users}))
+  }
+handleChange = (e) => {
+  this.setState({searchFlield: e.target.value});
+  };
+render(){
+  const {monsters,searchFlield} = this.state;
+  const fillteredMonsters = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchFlield.toLowerCase())
   );
+
+    return  (
+      <div className='App'>
+        <h1>Search your cats</h1>
+      <SearchBox
+        placeholder='Search Monsters'
+        handleChange = {this.handleChange}
+        />
+      <CardList monsters = {fillteredMonsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
